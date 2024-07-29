040b94b10efafe9adbaa30313fc1855ceb0a31f1
cliffordaustin <106864832+cliffordaustin@users.noreply.github.com>
2024-07-29 12:14:08 +0000

    updated code

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -10,15 +10,6 @@ function Post({ post }: { post: PostType }) {
       href={`/posts/${post.id}`}
       className="flex sm:flex-row flex-col gap-5 py-8 border-b border-gray-200 dark:border-[#bfc0c0]"
     >
-      <div className="relative w-full sm:w-[600px] md:w-[400px] h-[200px] sm:h-[150px]">
-        <Image
-          src={post.thumbnail}
-          alt={post.title}
-          className="object-cover"
-          fill
-        />
-      </div>
-
       <div className="flex flex-col gap-3">
         <div className="flex items-center gap-2 flex-wrap">
           {post.tags.map((tag) => (
@@ -33,6 +24,14 @@ function Post({ post }: { post: PostType }) {
         <div className="mt-2 dark:text-white text-gray-600">
           {formatDistance(post.createdAt, new Date(), { addSuffix: true })}
         </div>
+      </div>
+      <div className="relative w-full sm:w-[600px] md:w-[400px] h-[200px] sm:h-[150px]">
+        <Image
+          src={post.thumbnail}
+          alt={post.title}
+          className="object-cover"
+          fill
+        />
       </div>
     </Link>
   );
