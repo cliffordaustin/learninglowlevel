fb9dd7265c7df5cbef06bb58938502d831572964
cliffordaustin <106864832+cliffordaustin@users.noreply.github.com>
2024-10-14 11:45:48 +0000

    included new post an updated the tailwind prose class

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -8,18 +8,10 @@ import {
 import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
 import { serverClient } from "@/app/_trpc/serverClient";
 import rehypeRaw from "rehype-raw";
-import prisma from "@/db/db";
 import PostNavigate from "@/components/PostNavigate";
+import Image from "next/image";
 
 async function PostPage({ params }: { params: { id: string } }) {
-  const markdown = `
-
-  `;
-  // await prisma.post.update({
-  //   where: { id: params.id },
-  //   data: { content: markdown },
-  // });
-
   const post = await serverClient.getPostById(params.id);
 
   const CustomSyntaxHighlighter = React.forwardRef<
@@ -45,11 +37,16 @@ async function PostPage({ params }: { params: { id: string } }) {
         <Navbar></Navbar>
       </div>
 
-      <div className="dark:text-white max-w-[800px] mx-auto bg-gray-100 dark:bg-[#333533] h-full min-h-screen py-8 px-5 md:px-20">
+      <div className="dark:text-white max-w-[800px] overflow-hidden mx-auto bg-gray-100 dark:bg-[#333533] h-full min-h-screen py-8 px-5 md:px-20">
         <h1 className="font-bold text-2xl md:text-3xl">{post?.title}</h1>
+        <div className="relative w-[500px] h-[350px] my-8 mx-auto">
+          {post?.thumbnail && (
+            <Image src={post?.thumbnail} alt={post?.title || ""} fill />
+          )}
+        </div>
         <ReactMarkdown
           rehypePlugins={[rehypeRaw]}
-          className="max-w-full prose-img:mx-auto prose-img:object-cover prose-img:w-[500px] prose-img:sm:w-[500px] prose-img:h-[350px] prose-img:md:h-[430px] prose md:prose-lg dark:prose-invert prose-pre:p-0 prose-pre:bg-transparent"
+          className="max-w-full prose-a:block prose-a:line-clamp-1 prose-a:text-ellipsis prose-a:text-blue-400 prose-img:mx-auto prose-img:object-contain prose-img:w-[500px] prose-img:sm:w-[600px] prose-img:max-h-[350px] prose-img:md:max-h-[430px] prose md:prose-lg dark:prose-invert prose-pre:p-0 prose-pre:bg-transparent"
           components={{
             code(props) {
               const { children, className, node, ...rest } = props;
